5771e74fb26915c16b6b35489bbbf49b22d0a2cd
veena19nayak-droid <veena19nayak@gmail.com>
2025-12-12 19:23:17 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,1 +1,8 @@
+try:
+  a = float(input("Enter first number:"))
+  b = float(input("Enter second number:"))
 
+  print(f"Addition: {a+b}")
+  print(f"Subtraction: {a-b}")
+except ValueError:
+  print("Please enter valid numbers.")
